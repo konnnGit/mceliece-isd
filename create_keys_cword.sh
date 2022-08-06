01fc20229148306516c7e3f78a27471a2cad12a4
Konstantinos Spalas <konnn.sp@gmail.com>
2022-08-06 09:48:41 +0300

    finalizing.

--- create_keys_cword.sh
+++ create_keys_cword.sh
@@ -3,6 +3,6 @@ m=$1
 t=$2  
    
 ./McEliece.py -g -m $m -t $t -o $m$t -v
-txt='sasa'
+txt='Hello'
 ./MatrixCodec.py -e $txt -par $m$t.Hpub -v -o $m$t.binMsg    
 ./McEliece.py -e $m$t.binMsg   -pub $m$t.pub -o $m$t.codeword 
